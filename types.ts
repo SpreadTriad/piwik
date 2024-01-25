6d2bc67dea213341d6d327b97f4388c1c96f5f51
rogier83 <rogier@oberon.nl>
2024-01-25 12:56:40 +0100

    initial commit

---
+++ types.ts
@@ -0,0 +1,72 @@
+// Dit zijn de custom dimensions, niet sessie-gerelateerd.
+// NB: deze lijst is overgenomen uit aansluitgids en moeten we nog even nalopen: wat is hiervan een session dimension en wat niet?
+export enum PiwikDimension {
+  /** Gebruikerstype (al gebruikt door mijn.amsterdam als ProfileType) */
+  userType = 1,
+  /** Gebruikersstad (al gebruikt door mijn.amsterdam als City) */
+  userCity = 2,
+  /** Zoekterm gebruikt in zoekmachine */
+  searchTerm = 3,
+  /** Aantal zoekresultaten */
+  searchResultAmount = 4,
+  /** Soort zoekopdracht */
+  searchType = 5,
+  /** Title van het aangeklikte zoekresultaat */
+  searchResultTitle = 6,
+  /** URL van het aangeklikte zoekresultaat */
+  searchResultUrl = 7,
+  /** Soort van het aangeklikte zoekresultaat */
+  searchResultType = 8,
+  /** Het hoeveelste zoekresultaat is aangeklikt (telt van boven) */
+  searchResultSelected = 9,
+  /** Titel van het content bericht */
+  contentTitle = 10,
+  /** Onderwerp/Onderwerpen van het content bericht */
+  contentSubject = 11,
+  /** Team dat het content bericht heeft geschreven */
+  contentTeam = 12,
+  /** Laatste publicatiedatum van het content bericht */
+  contentPublicationDate = 13,
+  /** Type van de content (artikel/nieuws) */
+  contentType = 14,
+  /** Gebruikers filter op intern of extern */
+  userIntext = 15,
+  /** Zoekmachine soort */
+  searchMachine = 16,
+  /** Pagina type o.b.v. Amsterdam Content Meetmodel */
+  pageType = 17,
+}
+
+// We definieren de session dimensions apart, zodat we snappen wat we aan het loggen zijn.
+export enum PiwikSessionDimension {
+  reduceMotionEnabled = 20,
+  locationPermission = 21,
+  screenReaderEnabled = 22,
+}
+
+
+// In de app hebben we modules. We gebruiken categorie om aan te geven bij welke module een log hoort.
+// Voor zaken die niet onder een module vallen, gebruiken we "general".
+export enum PiwikCategory {
+  about = 'about',
+  address = 'address',
+  constructionWork = 'construction-work',
+  constructionWorkEditor = 'construction-work-editor',
+  contact = 'contact',
+  general = 'general',
+  home = 'home',
+  onboarding = 'onboarding',
+  openWasteContainer = 'open-waste-container',
+  redirects = 'redirects',
+  reportProblem = 'report-problem',
+  user = 'user',
+  wasteGuide = 'waste-guide',
+}
+
+// De acties die een log kunnen triggeren (wordt nog aangevuld).
+export enum PiwikAction {
+  addressAdd = 'addressAdd',
+  addressChange = 'addressChange',
+  buttonPress = 'buttonPress',
+  toForeground = 'toForeground',
+}
